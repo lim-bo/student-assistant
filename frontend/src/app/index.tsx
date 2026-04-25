04bfe0fb9287ecca17c04e12e52cd0e576d35aca
PadlockF <mrcld281@gmail.com>
2026-04-25 22:01:12 +0300

    delete old frontend

--- frontend/src/app/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-    return (
-        <div>
-            <h2 className="text-2xl font-bold">Домашняя страница</h2>
-            <p className="mt-2">Добро пожаловать!</p>
-        </div>
-    );
-}
